a6d87b0bceabd84ff438af18d07445655fb418ca
NichtsHsu <NichtsVonChaos@gmail.com>
2022-11-08 10:16:29 +0800

    use new clap version

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@
 
 mod game;
 mod interaction;
-use clap::{App, Arg};
+use clap::Parser;
 use std::{io::stdin, process};
+
+#[derive(Parser, Debug)]
+#[command(name = "Japanese Mahjong Theory Shell")]
+#[command(author = "Nichts Hsu <NichtsVonChaos@gmail.com>")]
+#[command(version = "1.18")]
+#[command(about = "Japanese Mahjong Theory Shell", long_about = None)]
+struct Args {
+    #[arg(short, long, help = "Set output format: standard | json", default_value_t = String::from("standard"))]
+    format_type: String,
+    #[arg(short, long, help = "Set players number: 3 | 4", default_value_t = 4)]
+    players_number: u8,
+    #[arg(short, help = "Start with interactive mode", long)]
+    interactive: bool,
+}
 
 fn main() {
     process::exit(match run_application() {
@@ -14,65 +28,27 @@ fn main() {
 
 fn run_application() -> Result<(), ()> {
     // Handle program arguments.
-    let matches = App::new("Japanese Mahjong Theory")
-        .version("1.18")
-        .author("Nichts Hsu <NichtsVonChaos@gmail.com>")
-        .arg(
-            Arg::with_name("format")
-                .short('f')
-                .long("format")
-                .takes_value(true)
-                .value_name("format_type")
-                .help("Set output format: standard, json"),
-        )
-        .arg(
-            Arg::with_name("players")
-                .short('p')
-                .long("player")
-                .takes_value(true)
-                .value_name("players_number")
-                .help("Set players number, 3 or 4"),
-        )
-        .arg(
-            Arg::with_name("interactive")
-                .short('i')
-                .long("interactive")
-                .help("Start with interactive mode"),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let output_format = if let Some(format_type) = matches.value_of("format") {
-        if format_type == "standard" {
-            interaction::OutputFormat::Standard
-        } else if format_type == "json" {
-            interaction::OutputFormat::Json
-        } else {
-            println!("Unknown format type: {}.", format_type);
-            return Err(());
-        }
-    } else {
+    let output_format = if args.format_type == "standard" {
         interaction::OutputFormat::Standard
+    } else if args.format_type == "json" {
+        interaction::OutputFormat::Json
+    } else {
+        println!("Unknown format type: {}.", args.format_type);
+        return Err(());
     };
 
-    let player_number = if let Some(players_number) = matches.value_of("players") {
-        if let Ok(players_number) = players_number.parse::<u32>() {
-            match players_number {
-                3 => game::PlayerNumber::Three,
-                4 => game::PlayerNumber::Four,
-                _ => {
-                    println!("Not support {}-players mode.", players_number);
-                    return Err(());
-                }
-            }
-        } else {
-            println!("Unparsed argument: {}.", players_number);
+    let player_number = match args.players_number {
+        3 => game::PlayerNumber::Three,
+        4 => game::PlayerNumber::Four,
+        _ => {
+            println!("Not support {}-players mode.", args.players_number);
             return Err(());
         }
-    } else {
-        game::PlayerNumber::Four
     };
 
-    let interactive = matches.is_present("interactive");
+    let interactive = args.interactive;
 
     // Initialize controller.
     let mut controller = interaction::Controller::new(output_format, player_number, interactive);
